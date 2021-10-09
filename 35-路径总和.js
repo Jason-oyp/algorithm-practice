688ac83af0913b35a9295a248cb9ac8fa85a5f4c
ouyangpan <1209899252@qq.com>
2021-10-09 22:59:55 +0800

    路径总和

---
+++ 35-路径总和.js
@@ -0,0 +1,7 @@
+var hasPathSum = function(root, targetSum) {
+    if (root === null) return false 
+    if (root.left === null && root.right === null) {
+        return targetSum === root.val
+    }
+    return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val)
+};
