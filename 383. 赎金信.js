f5e96ffa65f9ae8d9f4200ea8628911f7d8e5b11
ouyangpan <1209899252@qq.com>
2022-06-12 22:57:11 +0800

    赎金信第二种解法

--- 383. 赎金信.js
+++ 383. 赎金信.js
@@ -18,3 +18,21 @@
     }
     return true
 };
+
+
+var canConstruct = function(ransomNote, magazine) {
+    if (ransomNote.length > magazine.length) {
+        return false;
+    }
+    const cnt = new Array(26).fill(0);
+    for (const c of magazine) {
+        cnt[c.charCodeAt() - 'a'.charCodeAt()]++;
+    }
+    for (const c of ransomNote) {
+        cnt[c.charCodeAt() - 'a'.charCodeAt()]--;
+        if(cnt[c.charCodeAt() - 'a'.charCodeAt()] < 0) {
+            return false;
+        }
+    }
+    return true;
+};
