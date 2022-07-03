c9a8e364ca0a47febd925ab8b505432f36c3355a
ouyangpan <1209899252@qq.com>
2022-07-03 22:49:35 +0800

    559. N 叉树的最大深度

---
+++ 559. N 叉树的最大深度.js
@@ -0,0 +1,21 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (root === null) return 0;
+  const maxs = [];
+  for (let i = 0; i < root.children.length; i++) {
+    maxs.push(maxDepth(root.children[i]));
+  }
+  if (maxs.length === 0) return 1;
+  return Math.max(...maxs) + 1;
+};
