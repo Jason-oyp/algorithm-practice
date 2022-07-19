b3c422f2feb51c8bca064a5ba3285daf452751f7
ouyangpan <1209899252@qq.com>
2022-07-19 22:45:08 +0800

    762. 二进制表示中质数个计算置位

---
+++ 762. 二进制表示中质数个计算置位.js
@@ -0,0 +1,28 @@
+var countPrimeSetBits = function (left, right) {
+  let ans = 0;
+  for (let x = left; x <= right; ++x) {
+    if (isPrime(bitCount(x))) {
+      ++ans;
+    }
+  }
+  return ans;
+};
+
+const isPrime = (x) => {
+  if (x < 2) {
+    return false;
+  }
+  for (let i = 2; i * i <= x; ++i) {
+    if (x % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const bitCount = (x) => {
+  return x.toString(2).split("0").join("").length;
+};
+
+
+// 看看js如何判断有个数是否为质数
