4efbc06ae52940fb3a0fdeaa98ff260a7a67ed15
ouyangpan <1209899252@qq.com>
2022-08-15 22:09:17 +0800

    989. 数组形式的整数加法

---
+++ 989. 数组形式的整数加法.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} num
+ * @param {number} k
+ * @return {number[]}
+ */
+var addToArrayForm = function (num, k) {
+  return String(BigInt(num.join("")) + BigInt(k)).split("");
+};
+
+
+
+// 解法二：（官方）
+var addToArrayForm = function (num, k) {
+  const res = [];
+  const n = num.length;
+  for (let i = n - 1; i >= 0; --i) {
+    let sum = num[i] + (k % 10);
+    k = Math.floor(k / 10);
+    if (sum >= 10) {
+      k++;
+      sum -= 10;
+    }
+    res.push(sum);
+  }
+  for (; k > 0; k = Math.floor(k / 10)) {
+    res.push(k % 10);
+  }
+  res.reverse();
+  return res;
+};
