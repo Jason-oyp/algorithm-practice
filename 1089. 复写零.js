82ad015fec62be608fe950534511789f6832c8d8
ouyangpan <1209899252@qq.com>
2022-08-23 21:46:08 +0800

    1089. 复写零

---
+++ 1089. 复写零.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function (arr) {
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    if (arr[i] === 0) {
+      arr.splice(i + 1, 0, 0);
+      i++;
+    }
+  }
+  arr.length = len;
+};
