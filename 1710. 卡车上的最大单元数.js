44787b2066e0e0a528184ef72d4097ce3dd34732
ouyangpan <1209899252@qq.com>
2022-11-26 16:48:33 +0800

    fix: 1710. 卡车上的最大单元数

--- 1710. 卡车上的最大单元数.js
+++ 1710. 卡车上的最大单元数.js
@@ -13,3 +13,37 @@ var maximumUnits = function (boxTypes, truckSize) {
   }
   return res;
 };
+
+var maximumUnits = function(boxTypes, truckSize) {
+    boxTypes.sort((a, b) => b[1] - a[1]);
+    let res = 0;
+    for (const boxType of boxTypes) {
+        let numberOfBoxes = boxType[0];
+        let numberOfUnitsPerBox = boxType[1];
+        if (numberOfBoxes < truckSize) {
+            res += numberOfBoxes * numberOfUnitsPerBox;
+            truckSize -= numberOfBoxes;
+        } else {
+            res += truckSize * numberOfUnitsPerBox;
+            break;
+        }
+    }
+    return res;
+};
+
+var maximumUnits = function (boxTypes, truckSize) {
+    boxTypes.sort((a, b) => b[1] - a[1]);
+    let res = 0;
+    for (const boxType of boxTypes) {
+        const numberOfBoxes = boxType[0];
+        const numberOfUnitsPerBox = boxType[1];
+        if (numberOfBoxes < truckSize) {
+            res += numberOfBoxes * numberOfUnitsPerBox;
+            truckSize -= numberOfBoxes;
+        } else {
+            res += truckSize * numberOfUnitsPerBox;
+            break
+        }
+    }
+    return res;
+};
