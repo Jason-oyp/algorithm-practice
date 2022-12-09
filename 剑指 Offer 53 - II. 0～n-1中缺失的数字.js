3579768ad9cd0d2388a34f17843d8dc3ec10d8ab
ouyangpan <1209899252@qq.com>
2022-12-09 10:05:13 +0800

    剑指 Offer 53 - II. 0～n-1中缺失的数字

---
+++ 剑指 Offer 53 - II. 0～n-1中缺失的数字.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (i !== nums[i]) {
+      return i;
+    }
+  }
+  return nums.length;
+};
