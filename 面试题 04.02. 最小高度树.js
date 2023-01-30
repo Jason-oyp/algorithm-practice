2a66ddfbcee205275bff59be181545adf2a0e968
ouyangpan <1209899252@qq.com>
2023-01-30 10:20:23 +0800

    面试题 04.02. 最小高度树

---
+++ 面试题 04.02. 最小高度树.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function (nums) {
+  if (!nums.length) return null;
+  const middle = (nums.length - 1) >>> 1;
+  const root = new TreeNode(nums[middle]);
+  root.left = sortedArrayToBST(nums.slice(0, middle));
+  root.right = sortedArrayToBST(nums.slice(middle + 1));
+  return root;
+};
