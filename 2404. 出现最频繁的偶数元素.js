0b98dee0367bd5ae476a2a4e84412bb37c4a87b6
ouyangpan <1209899252@qq.com>
2023-06-13 19:45:28 +0800

    2404. 出现最频繁的偶数元素

--- 2404. 出现最频繁的偶数元素.js
+++ 2404. 出现最频繁的偶数元素.js
@@ -19,3 +19,25 @@ var mostFrequentEven = function (nums) {
   }
   return -1;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var mostFrequentEven = function (nums) {
+  const map = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (!(nums[i] & 1)) {
+      if (map[nums[i]]) {
+        map[nums[i]]++;
+      } else {
+        map[nums[i]] = 1;
+      }
+    }
+  }
+  const arr = Object.entries(map).sort((a, b) => b[1] - a[1]);
+  if (arr.length) {
+    return arr[0][0];
+  }
+  return -1;
+};
