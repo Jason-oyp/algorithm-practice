db83e03dfceb1a859c180e313ea7003fbbcde687
ouyangpan <1209899252@qq.com>
2023-06-21 10:42:34 +0800

    2446. 判断两个事件是否存在冲突

---
+++ 2446. 判断两个事件是否存在冲突.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string[]} event1
+ * @param {string[]} event2
+ * @return {boolean}
+ */
+var haveConflict = function (event1, event2) {
+  function compare(s1, s2, type) {
+    let result;
+    const e11 = +s1.slice(0, 2);
+    const e12 = +s1.slice(3);
+    const e21 = +s2.slice(0, 2);
+    const e22 = +s2.slice(3);
+    if (e11 === e21) {
+      if (e12 < e22) {
+        result = type === "max" ? s2 : s1;
+      } else {
+        result = type === "max" ? s1 : s2;
+      }
+    } else {
+      if (type === "max") {
+        result = e11 > e21 ? s1 : s2;
+      } else {
+        result = e11 > e21 ? s2 : s1;
+      }
+    }
+    return result;
+  }
+  const max = compare(event1[0], event2[0], "max");
+  const min = compare(event1[1], event2[1], "min");
+  return max > min ? false : true;
+};
