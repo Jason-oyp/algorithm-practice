68ae539fc69a84316ee9c53beee5f40fa933fed5
ouyangpan <1209899252@qq.com>
2023-08-25 10:00:00 +0800

    2715. 执行可取消的延迟函数

---
+++ 2715. 执行可取消的延迟函数.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function (fn, args, t) {
+  const timer = setTimeout(() => {
+    fn(...args);
+  }, t);
+  return () => {
+    clearTimeout(timer);
+  };
+};
