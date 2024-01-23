04a4518fb3b5628184dc0e4a99ce716585e38602
ouyangpan <ouyangpan@hydee.cn>
2024-01-23 22:09:19 +0800

    2960. 统计已测试设备

---
+++ 2960. 统计已测试设备.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} batteryPercentages
+ * @return {number}
+ */
+var countTestedDevices = function (batteryPercentages) {
+  let ans = 0;
+  for (let i = 0; i < batteryPercentages.length; i++) {
+    if (batteryPercentages[i] > 0) {
+      ans++;
+      for (let j = i + 1; j < batteryPercentages.length; j++) {
+        batteryPercentages[j]--;
+      }
+    }
+  }
+  return ans;
+};
